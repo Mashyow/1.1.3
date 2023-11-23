47874d049808d56415f1c63799a4fcb107c20f99
Mashyow <mashyowrf@gmail.com>
2023-11-23 18:43:59 +0300

    Исправлено

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,25 +1,25 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.dao.UserDao;
-import jm.task.core.jdbc.dao.UserDaoHibernateImpl;
+import jm.task.core.jdbc.service.UserService;
+import jm.task.core.jdbc.service.UserServiceImpl;
 
 public class Main {
 
 
     public static void main(String[] args) {
-        UserDao userDao = new UserDaoHibernateImpl();
+        UserService userService = new UserServiceImpl();
 
-        userDao.createUsersTable();
+        userService.createUsersTable();
 
-        userDao.saveUser("Den", "Mashyow", (byte) 19);
-        userDao.saveUser("Lola", "Opa", (byte) 20);
-        userDao.saveUser("Opa", "Lolaeva", (byte) 22);
-        userDao.saveUser("Lersi", "Isrelova", (byte) 25);
+        userService.saveUser("Den", "Mashyow", (byte) 19);
+        userService.saveUser("Lola", "Opa", (byte) 20);
+        userService.saveUser("Opa", "Lolaeva", (byte) 22);
+        userService.saveUser("Lersi", "Isrelova", (byte) 25);
 
-        userDao.removeUserById(1);
-        userDao.getAllUsers();
-        userDao.cleanUsersTable();
-        userDao.dropUsersTable();
+        userService.removeUserById(1);
+        userService.getAllUsers();
+        userService.cleanUsersTable();
+        userService.dropUsersTable();
 
     }
 }
